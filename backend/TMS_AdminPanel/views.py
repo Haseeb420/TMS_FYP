f0a707726af049014c82af85176db764501fb297
Muhammad Haseeb Malik <63384019+Haseeb420@users.noreply.github.com>
2021-11-29 14:56:17 +0500

    commit on 29-11-2021
    commit on 29-11-2021

--- backend/TMS_AdminPanel/views.py
+++ backend/TMS_AdminPanel/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request,'AdminSide/index.html',{"title":"Royal Travels"})
 
+def index(request):
+    return render(request, 'AdminSide/templates/index.html', {"title": "Royal Travels"})
+
+
+def packages(request):
+    return render(request, 'AdminSide/templates/Packages/Packages.html',
+                  {"title": "Royal Travels"})
