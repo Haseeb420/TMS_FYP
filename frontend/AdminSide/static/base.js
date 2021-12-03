9e26719e5a88d9cf13dedca388a505e99bdf5ca0
Zara <zarawaheed135@gmail.com>
2021-12-04 02:17:56 +0500

    setup project with front end pages

---
+++ frontend/AdminSide/static/base.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDEMvj0PEtM82BOWzXDMO72Yl3gH0vPGOQ",
+    authDomain: "wherks-e246c.firebaseapp.com",
+    projectId: "wherks-e246c",
+    storageBucket: "wherks-e246c.appspot.com",
+    messagingSenderId: "104239160220",
+    appId: "1:104239160220:web:0c40362c91b7c20930c659",
+    measurementId: "G-SY3G1L5RHP"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
