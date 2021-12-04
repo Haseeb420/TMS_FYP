7ecd43b4f43081351155774ea9ae69c2175ce895
Muhammad Haseeb Malik <63384019+Haseeb420@users.noreply.github.com>
2021-12-04 21:45:26 +0500

    update .gitignore

--- backend/TMS_AdminPanel/views.py
+++ backend/TMS_AdminPanel/views.py
@@ -2,32 +2,44 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 # Create your views here.
 
+
 class IndexView(View):
     template_name = "AdminSide/templates/dashboard.html"
-    def get(self,request):
+
+    def get(self, request):
         return render(request, self.template_name)
+
+
 class PackageView(View):
     template_name = "AdminSide/templates/Packages/Packages.html"
-    def get(self,request):
-       return render(request, self.template_name)
+
+    def get(self, request):
+        return render(request, self.template_name)
+
 
 class ProductView(View):
     template_name = "AdminSide/templates/product_list.html"
-    def get(self,request):
-       return render(request, self.template_name)
+
+    def get(self, request):
+        return render(request, self.template_name)
+
 
 class AddProductView(View):
     template_name = "AdminSide/templates/add_product.html"
-    def get(self,request):
-       return render(request, self.template_name)
+
+    def get(self, request):
+        return render(request, self.template_name)
+
 
 class EditProductView(View):
     template_name = "AdminSide/templates/edit_product.html"
-    def get(self,request):
-       return render(request, self.template_name)
+
+    def get(self, request):
+        return render(request, self.template_name)
+
 
 class DashboardView(View):
     template_name = "AdminSide/templates/dashboard.html"
-    def get(self,request):
-       return render(request, self.template_name)
 
+    def get(self, request):
+        return render(request, self.template_name)
