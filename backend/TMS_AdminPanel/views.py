5214b5488787801c3f2facb5d76fdaded24de14b
Muhammad Haseeb Malik <63384019+Haseeb420@users.noreply.github.com>
2021-12-04 21:34:39 +0500

    error

--- backend/TMS_AdminPanel/views.py
+++ backend/TMS_AdminPanel/views.py
@@ -2,32 +2,50 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 # Create your views here.
 
+_SiteName = "-Royal Travels"
+
+
 class IndexView(View):
-    template_name = "AdminSide/templates/dashboard.html"
-    def get(self,request):
-        return render(request, self.template_name)
+    _template_name = "AdminSide/templates/dashboard.html"
+    _title = "Dashboard"+_SiteName
+
+    def get(self, request):
+        data = {"title": self._title}
+        return render(request, self._template_name, data)
+
+
 class PackageView(View):
     template_name = "AdminSide/templates/Packages/Packages.html"
-    def get(self,request):
-       return render(request, self.template_name)
+    _title = "Packages"+_SiteName
+
+    def get(self, request):
+        data = {"title": self._title}
+        return render(request, self.template_name, data)
+
 
 class ProductView(View):
     template_name = "AdminSide/templates/product_list.html"
-    def get(self,request):
-       return render(request, self.template_name)
+
+    def get(self, request):
+        return render(request, self.template_name)
+
 
 class AddProductView(View):
     template_name = "AdminSide/templates/add_product.html"
-    def get(self,request):
-       return render(request, self.template_name)
+
+    def get(self, request):
+        return render(request, self.template_name)
+
 
 class EditProductView(View):
     template_name = "AdminSide/templates/edit_product.html"
-    def get(self,request):
-       return render(request, self.template_name)
+
+    def get(self, request):
+        return render(request, self.template_name)
+
 
 class DashboardView(View):
     template_name = "AdminSide/templates/dashboard.html"
-    def get(self,request):
-       return render(request, self.template_name)
 
+    def get(self, request):
+        return render(request, self.template_name)
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/',include("TMS_AdminPanel.urls")),
-    path('',include("TMS_Views.urls")),
-    
+    path('admin/', include("TMS_AdminPanel.urls")),
+    path('', include("TMS_Views.urls")),
+
 ]
